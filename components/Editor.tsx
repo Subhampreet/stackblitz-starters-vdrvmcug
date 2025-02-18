7396c4108bd9b65cffafbf713f814f27e004ed16
Subhampreet <subhampreet18@gmail.com>
2025-02-18 20:18:37 +0530

    Build 0.0.3 : UI Code Editor Complete

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,10 +1,15 @@
 "use client"
 
 import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
 
 const Editor = () => {
     return (<div className="h-full w-6/12 relative">
-        <CodeMirror minHeight='100vh' value='Hello World' onChange={() => {}} theme={'dark'} />
+        <CodeMirror minHeight='100vh' value='Hello World' onChange={() => {}} theme={'dark'} extensions={[javascript({ jsx: true })]} 
+            style={{
+                fontSize: "30px"
+            }}
+        />
     </div>)
 }
 
