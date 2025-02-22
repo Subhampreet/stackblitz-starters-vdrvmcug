c1c56e25e34b9e5f5f52f6bd3292f208046caf6f
Subhampreet <subhampreet18@gmail.com>
2025-02-22 23:47:35 +0530

    Build 0.0.7 : Server Setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
