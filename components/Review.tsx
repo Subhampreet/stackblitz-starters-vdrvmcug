be9b6eab60a2af6f534323a4d498905da0962734
Subhampreet Mohanty <subhampreet18@gmail.com>
2025-03-05 19:00:35 +0530

    Update Review.tsx

--- components/Review.tsx
+++ components/Review.tsx
@@ -1,16 +1,26 @@
-"use client"
+import MarkdownPreview from "@uiw/react-markdown-preview";
 
-import MarkdownPreview from '@uiw/react-markdown-preview';
-
+import Loader from "./Loader";
 
 const Review = ({
-    review,
-  }: {
-    review: string;
-  }) => {
-    return (<div className="h-full w-6/12 relative">
-        <MarkdownPreview source={review} style={{ fontSize: 20, height: "100vh" }} />
-    </div>)
-}
+  review,
+  isGenerating,
+}: {
+  review: string;
+  isGenerating: boolean;
+}) => {
+  return (
+    <div className="h-full w-6/12 relative overflow-scroll">
+      {isGenerating ? (
+        <Loader />
+      ) : (
+        <MarkdownPreview
+          source={review}
+          style={{ fontSize: "30px", minHeight: "100vh", padding: "20px" }}
+        />
+      )}
+    </div>
+  );
+};
 
-export default Review
+export default Review;
