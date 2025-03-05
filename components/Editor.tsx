257d059bf0f24b246d1c45565d1959bdb188086c
Subhampreet Mohanty <subhampreet18@gmail.com>
2025-03-05 18:59:44 +0530

    Update Editor.tsx

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,32 +1,37 @@
-"use client"
-
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
-import { useState } from 'react';
+import { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 
 const Editor = ({
-    onGenerateReview,
-  }: {
-    onGenerateReview(code: string): void;
-  }) => {
+  isGenerating,
+  onGenerateReview,
+}: {
+  isGenerating: boolean;
+  onGenerateReview(code: string): void;
+}) => {
+  const [code, setCode] = useState("");
 
-    const [code, setCode] = useState("");
-
-
-    return (<div className="h-full w-6/12 relative">
-        <button
-        onClick={onGenerateReview}
+  return (
+    <div className="h-full w-6/12 relative">
+      <button
+        disabled={isGenerating}
+        onClick={() => onGenerateReview(code)}
         className="w-max absolute bottom-3 right-3 z-50 bg-green-500 p-2 rounded hover:bg-green-700 active:translate-y-1 disabled:opacity-75  disabled:pointer-events-none disabled:cursor-not-allowed"
       >
         Generate Review
       </button>
-        <CodeMirror minHeight='100vh'  value={code}
-        onChange={setCode} theme={'dark'} extensions={[javascript({ jsx: true })]} 
-            style={{
-                fontSize: "20px"
-            }}
-        />
-    </div>)
-}
+      <CodeMirror
+        minHeight="100vh"
+        value={code}
+        onChange={setCode}
+        theme="dark"
+        extensions={[javascript({ jsx: true })]}
+        style={{
+          fontSize: "30px",
+        }}
+      />
+    </div>
+  );
+};
 
-export default Editor
+export default Editor;
